9567ff0927df7262bc92d5e0191fc9d3a489910a
Christian Heinrich <christian.heinrich@digitalcareerinstitute.org>
2021-11-16 13:47:39 +0100

    Update der Lösung

---
+++ christian.js
@@ -0,0 +1,12 @@
+let myNumber = 5;
+// myNumber = -2;
+
+if (myNumber >= 0) {
+  // alles hier drin wird ausgeführt, wenn die if-Bedingung
+  // *richtig* ist, bzw. erfüllt wird.
+  console.log(myNumber, "ist eine positive Zahl");
+} else {
+  // else wird automatisch ausgeführt, wenn die if-Bedingung
+  // *nicht* erfüllt ist
+  console.log(myNumber, "ist eine negative Zahl");
+}

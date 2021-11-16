da5ca9205e3fb1e4ea80c81f01df6218403a0bce
Sharfuddin Ahmed <Sharfuddinahmed07@gmail.com>
2021-11-16 13:59:32 +0100

    its Done

---
+++ sharfuddin.js
@@ -0,0 +1,11 @@
+let myNumber = 5;
+//myNumber= -2;
+
+if (myNumber >= 0) {
+    console.log(myNumber,"ist eine positive Zahlt");
+//ausgeführt () wahr ist
+} else {
+    // else wird automatisch ausgefuhrt, wenn die if-Bedingung
+    //*nicht* erfühllt ist
+    console.log(myNumber, "ist eine negative Zahl");
+}

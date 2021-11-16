4527aec68f06dff65b4c3e58c48958affd0480ca
Enza <86748625+Enza-von-metaTronics@users.noreply.github.com>
2021-11-16 13:31:24 +0100

    besser?

--- PB-12-isNumBiggerThan-enzs.js
+++ PB-12-isNumBiggerThan-enzs.js
@@ -1,12 +1,11 @@
 //1.)
-let myNumber = 8;
+let myNumber = -8;
 //2.)
 console.log(myNumber);
 //3.)
 if (myNumber >= 0) {
     console.log(myNumber ,"ist eine positive Zahl")
-};
-//4.)
-if (myNumber <= 0) {
+} else {
     console.log(myNumber , "ist eine negative Zahl")
-}
+};
+

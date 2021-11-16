90b94d55e876ea53b38790ab36e8c500b005909f
slavablack <veaceslav.ciornii@magenta.de>
2021-11-16 13:17:49 +0100

    Zahl Veaceslav

---
+++ zahl.js
@@ -0,0 +1,19 @@
+let myNumber=7;
+if(myNumber>=0) {
+    console.log (
+        "myNumber ist eine positive Zahl"
+    )
+}
+else{
+    console.log("myNumer ist eine negative Zahl")
+};
+
+let mynegativesNumber=-7;
+if(mynegativesNumber>=0) {
+    console.log (
+        "myNumber ist eine positive Zahl"
+    )
+}
+else{
+    console.log("myNumer ist eine negative Zahl")
+};

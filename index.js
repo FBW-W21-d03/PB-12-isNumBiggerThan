d8238ca892ef25d7a752c5501c86531c56e2b9f8
kawanaza <kawanbabani@gmail.com>
2021-11-16 13:39:24 +0100

    done

---
+++ index.js
@@ -0,0 +1,19 @@
+
+let myNumber=4;
+if(myNumber>=0) {
+    console.log ("myNumber ist eine positive Zahl")
+}
+else{
+    console.log("myNubmer ist eine negative Zahl");
+}
+
+
+let myNegativesNumber=-4;
+
+if( myNumber<=0) {
+    console.log ( "myNumber ist eine positive Zahl")
+           
+}
+else{
+    console.log("myNumer ist eine negative Zahl");
+}

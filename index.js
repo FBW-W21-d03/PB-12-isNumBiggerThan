f67426159ea7c44b3cd50ce39cfee847d07257b4
sprotte <metatron1972@live.de>
2021-11-16 13:13:20 +0100

    sprotte

---
+++ index.js
@@ -0,0 +1,15 @@
+let foo1 = 4; 
+let foo2 = 12.8;
+console.log (foo1 + foo2);
+
+function testNum(a) {
+    let result;
+    if (a > 0) {
+      result = 'richtig';
+    } else {
+      result = 'falsch';
+    }
+    return result;
+  }
+  
+  console.log(testNum(-5));

92258273ab28ec05fec33e8ab5660fdcf5248b2a
Mohal1125 <rajwindersinghmohal@yahoo.com>
2021-11-16 13:21:29 +0100

    fertig

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ let myNumber = 6;
 
 console.log(myNumber);
 
-if (myNumber >= 8) {
+if (myNumber >= 0) {
     console.log(myNumber ,"ist eine positive Zahl")
 };
 
-if (myNumber <= 8) {
+if (myNumber <= 0) {
     console.log(myNumber , "ist eine negative Zahl")
 }

8adfbc63076c7c19a39cc6f09cacdb6749b4b1fc
Hayat Alkheder <hayat.alkheder@gmail.com>
2021-11-16 13:48:57 +0100

    if statements

---
+++ index.js
@@ -0,0 +1,13 @@
+
+let myNumber = 2; 
+if (myNumber  >= 0){
+    console.log(myNumber,"der Zahl einen positiven Wert");
+}else
+{
+    console.log(myNumber,"my Numer ist eine negative Zahl");
+}
+
+
+
+
+ 

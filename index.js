3197dafcb8af19098a72c2ba2b1a778e1b860cfc
Mohal1125 <rajwindersinghmohal@yahoo.com>
2021-11-16 13:18:10 +0100

    fertig

---
+++ index.js
@@ -0,0 +1,12 @@
+
+let myNumber = 6;
+
+console.log(myNumber);
+
+if (myNumber >= 8) {
+    console.log(myNumber ,"ist eine positive Zahl")
+};
+
+if (myNumber <= 8) {
+    console.log(myNumber , "ist eine negative Zahl")
+}

61384367f5389d6ce221faaf464ed1704b9b542d
Dennisbullys <d.rochus@hotmail.de>
2021-11-16 13:19:36 +0100

    Dennis

---
+++ Dennis.js
@@ -0,0 +1,17 @@
+console.log("isNumBiggerThan");
+
+
+// 1. schreibe eine Variable myNumber und vergebe der Zahl einen positiven Wert.
+let myNumber = 5;
+
+if (myNumber >= 0) {
+  console.log("myNumber ist eine positive Zahl ");
+} else {
+  console.log("myNumer ist eine negative Zahl");
+}
+
+
+
+
+
+

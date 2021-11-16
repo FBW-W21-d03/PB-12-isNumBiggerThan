476e351f0df57b4a6a025bc5b138bd9ed992bdd4
Mazen <mazen.taha88@gmail.com>
2021-11-16 13:24:57 +0100

    My lösung

---
+++ Mazen.js
@@ -0,0 +1,31 @@
+
+// Fall (1)
+let myNumber = 1 ;
+
+if(myNumber= 0) {
+
+    myNumber => 0;
+
+    console.log("myNumber ist eine positive Zahl");
+
+} else{
+
+    myNumber < 0;
+
+     console.log("myNumer ist eine negative Zahl");
+}
+
+// Fall (2)
+
+if(myNumber= 1) {
+
+    myNumber => 0;
+
+    console.log("myNumber ist eine positive Zahl");
+
+} else{
+
+    myNumber < 0;
+
+     console.log("myNumer ist eine negative Zahl");
+}
